0e891d1df2ca3ad3549fbb870dff3fc38b795dd0
Haruka <hfujii1013@gmail.com>
2023-04-10 00:07:20 -0700

    added section 13-170 Error Boundaries

--- Section13/src/components/UserFinder.js
+++ Section13/src/components/UserFinder.js
@@ -2,14 +2,12 @@ import { Fragment, useState, useEffect, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UserContext from "../store/users-context";
+import ErrorBoundry from "./ErrorBoundry";
 
 class UserFinder extends Component {
+  static contextType = UserContext;
+
   constructor() {
     super();
     this.state = {
@@ -20,13 +18,13 @@ class UserFinder extends Component {
 
   componentDidMount() {
     //Send http request...
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
@@ -43,7 +41,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundry>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundry>
       </Fragment>
     );
   }
@@ -72,3 +72,22 @@ class UserFinder extends Component {
 // };
 
 export default UserFinder;
+
+//memo1
+//The "componentDidMount()" method is called automatically by React after the component has been
+//rendered to the DOM. In this method, an HTTP request could be sent to fetch data, but in this case,
+//it simply sets the initial state of "filteredUsers" with the "DUMMY_USERS" array
+
+//memo2
+//The "componentDidUpdate()" method is called automatically by React whenever the component updates due to changes in props or state.
+//In this method, it checks if the "searchTerm" in the current state is different from the "searchTerm" in the previous state. If they are different,
+//it updates the "filteredUsers" state by filtering the "DUMMY_USERS" array based on the new "searchTerm".
+
+//In the provided code, the "filteredUsers" state is updated using the "DUMMY_USERS.filter()" method,
+//which is a built-in method in JavaScript that creates a new array with all elements that pass a certain condition.
+//The condition is defined as a callback function that is provided as an argument to the "filter()" method.
+//The callback function iterates through each element in the "DUMMY_USERS" array and checks if the "name" property of the user object includes the value of "this.state.searchTerm". The "includes()" method is another built-in method in JavaScript that checks if a string includes a given substring. If the "name" includes the "searchTerm", the callback function returns "true" and the user object is included in the new filtered array, otherwise it returns "false" and the user object is excluded from the filtered array.
+
+//Finally, the "filteredUsers" state is updated with the new filtered array,
+//effectively filtering the "DUMMY_USERS" array based on the value of "this.state.searchTerm".
+//This is a common pattern used to implement filtering functionality in React components, where the UI is dynamically updated based on the changes in the state value.
