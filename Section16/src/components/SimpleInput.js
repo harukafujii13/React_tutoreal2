3a453a1746cfc6ded116a190187b22d27c4df37c
Haruka <hfujii1013@gmail.com>
2023-04-17 23:32:15 -0700

    added section 16-202

--- Section16/src/components/SimpleInput.js
+++ Section16/src/components/SimpleInput.js
@@ -1,9 +1,61 @@
+import { useEffect, useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enterdNameTouched, setEnterdNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is valid!");
+    }
+  }, [enteredNameIsValid]);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnterdNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName);
+    const entaredValue = nameInputRef.current.value;
+    console.log(entaredValue);
+
+    // nameInputRef.current.value = ""; => Not ideal, Don't manipulate the dom
+    setEnteredName("");
+  };
+
+  const nameInputIsInvalid = !enteredNameIsValid && enterdNameTouched;
+
+  const namInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={namInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
